d5c4cd10dd41dddafb3069338c6a66ed7e7c3041
Khusamov Sukhrob <khusamov@yandex.ru>
2017-09-27 21:07:08 +0300

    Выполнено пробное взаимодействие сервера и клиента и проба отладки

--- client/studio/app/store/Personnel.js
+++ client/studio/app/store/Personnel.js
@@ -7,15 +7,18 @@ Ext.define('Studio.store.Personnel', {
         'name', 'email', 'phone'
     ],
 
-    data: { items: [
-        { name: 'Jean Luc', email: "jeanluc.picard@enterprise.com", phone: "555-111-1111" },
-        { name: 'Worf',     email: "worf.moghsson@enterprise.com",  phone: "555-222-2222" },
-        { name: 'Deanna',   email: "deanna.troi@enterprise.com",    phone: "555-333-3333" },
-        { name: 'Data',     email: "mr.data@enterprise.com",        phone: "555-444-4444" }
-    ]},
+    // data: { items: [
+    //     { name: 'Jean Luc', email: "jeanluc.picard@enterprise.com", phone: "555-111-1111" },
+    //     { name: 'Worf',     email: "worf.moghsson@enterprise.com",  phone: "555-222-2222" },
+    //     { name: 'Deanna',   email: "deanna.troi@enterprise.com",    phone: "555-333-3333" },
+    //     { name: 'Data',     email: "mr.data@enterprise.com",        phone: "555-444-4444" }
+    // ]},
+
+    autoLoad: true,
 
     proxy: {
-        type: 'memory',
+        type: 'ajax',
+        url: 'http://localhost:3000/personnel',
         reader: {
             type: 'json',
             rootProperty: 'items'
--- server/index.ts
+++ server/index.ts
@@ -1,10 +1,31 @@
 
 import * as Express from 'express';
+import * as Cors from 'cors';
 
 const app = Express();
+app.use(Cors());
 
 app.get('/', (req, res) => {
 	res.send('куку');
 });
 
-app.listen(3000);
+
+app.get('/personnel', (req, res) => {
+	const data = [
+		{ name: 'Jean Luc', email: "jeanluc.picard@enterprise.com", phone: "555-111-1111" },
+		{ name: 'Worf',     email: "worf.moghsson@enterprise.com",  phone: "555-222-2222" },
+		{ name: 'Deanna',   email: "deanna.troi@enterprise.com",    phone: "555-333-3333" },
+		{ name: 'Data',     email: "mr.data@enterprise.com",        phone: "555-444-4444" }
+	];
+	res.json({
+		items: data
+	});
+})
+
+
+
+const port = 3000;
+
+app.listen(port, none => {
+	console.log(`Сервер Кардинал Студио слушает на порту '${port}'.`);
+});
