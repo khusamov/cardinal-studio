e705cec3f8698deb1a945ef008fd32d2ffbfc99b
Khusamov Sukhrob <khusamov@yandex.ru>
2018-01-05 17:28:40 +0300

    Добавлен комментарий для разработчика

--- client/studio/overrides/development/data/schema/Schema.js
+++ client/studio/overrides/development/data/schema/Schema.js
@@ -1,4 +1,7 @@
 
+/**
+ * Специальный оверрайд для замены urlPrefix во время разработки приложения.
+ */
 Ext.define('Studio.override.data.schema.Schema', {
 
 	override: 'Studio.data.schema.Schema',
