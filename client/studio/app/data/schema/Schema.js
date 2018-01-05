3ddece16c48bc3e0d4a3dea4575caa6f959daa6c
Khusamov Sukhrob <khusamov@yandex.ru>
2018-01-05 17:31:00 +0300

    Мелкие правки

--- client/studio/app/data/schema/Schema.js
+++ client/studio/app/data/schema/Schema.js
@@ -4,7 +4,7 @@ Ext.define('Studio.data.schema.Schema', {
 	extend: 'Ext.data.schema.Schema',
 	alias: 'schema.cardinalstudio',
 	type: 'cardinalstudio',
-	requires: ['Ext.data.proxy.Rest'],
+	requires: ['Ext.data.proxy.Rest', 'Ext.data.reader.Json'],
 
 	namespace: 'Studio.model',
 
